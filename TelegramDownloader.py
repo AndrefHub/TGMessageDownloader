b898aa1d0fdd5c4e9070fd2a456f10740eecedca
AndrefHub <andrew02541632@gmail.com>
2024-10-12 16:58:14 +0300

    Initial commit

---
+++ TelegramDownloader.py
@@ -0,0 +1,518 @@
+import os
+import argparse
+import configparser
+import asyncio
+import aiohttp
+import random
+import re
+import string
+import json
+import time
+import shutil
+import codecs
+from datetime import datetime
+from telethon import TelegramClient, events, connection
+import pathlib
+import logging
+import emoji
+
+log_folder = "logs"
+# create_output_directories(log_folder)
+logging.basicConfig(
+    # filename=f"{log_folder}/telegram_dler.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(name)-25s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(f"{log_folder}/telegram_dler.log"),
+        logging.StreamHandler(),
+    ],
+)
+
+logger = logging.getLogger(__name__)
+logging.getLogger("telethon").setLevel(logging.WARNING)
+#################### UTILS START
+
+
+def create_output_directories(*args):
+    for path in args:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+
+def write_messages_to_file(messages, filename):
+    with codecs.open(filename, "w", "utf-8") as f:
+        json.dump(messages, f, ensure_ascii=False)
+
+
+# Функция для проверки хэштегов с форматированием
+def has_valid_hashtag(text: str, hashtags: list[str]):
+    formatted_text = re.sub(
+        r"[*_~]", "", text
+    )  # Убираем форматирование (курсив, жирный и т.д.)
+    return any(hashtag in formatted_text for hashtag in hashtags)
+
+
+def check_message_text_for_hashtags(text: str, hashtags: list[str]):
+    if text:
+        # logger.warn(text)
+        return has_valid_hashtag(text, hashtags)
+    # skipping check for messages with no text
+    # because they are highly likely grouped media
+    return True
+
+
+def convert_message_to_data(message):
+    return {
+        "groupID": message.group_id or message.id,
+        "date": message.date,
+        "text": cleanup_text(message.text) or "",
+        "media": [message.media],
+    }
+
+
+def convert_group_to_data(group):
+    return {
+        "groupID": group[0].group_id,
+        "date": next(
+            message.date for message in group if message.date
+        ),  # get first available date if first message don't have one
+        "text": cleanup_text(
+            next((message.text for message in group if message.text), "")
+        ),
+        "media": [message.media for message in group if message.media],
+    }
+
+
+async def send_to_api(url, data):
+    logger.debug(f"Sending {data} to {url}")
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, json=data) as response:
+                if response.ok:
+                    logger.debug(f"Message successfully sent to Next.js API: {data}")
+                else:
+                    logger.warn(
+                        f"Failed to send message: {response.status}, {await response.text()}"
+                    )
+        except Exception as e:
+            logger.warn(f"Error sending message to API: {e}. Data: {data}")
+
+
+def is_media_downloaded(message_id, *paths):
+    for path in paths:
+        files = list(pathlib.Path(path).glob(f"{message_id}.*"))
+        if files:
+            return files
+    return []
+
+
+# Function to remove italic text but preserve hashtags
+def remove_italics(text):
+    # Function to preserve hashtags within italics
+    def preserve_hashtags(match):
+        content = match.group(0)  # Get the full matched text, including underscores
+        hashtags = re.findall(r"#\w+", content)  # Find hashtags inside the italics
+        return " ".join(hashtags)  # Return only the hashtags
+
+    # Substitute italic text while preserving hashtags
+    return re.sub(r"__.*?__", preserve_hashtags, text, flags=re.DOTALL)
+
+
+# Function to remove emojis
+def remove_emojis(text):
+    return emoji.replace_emoji(text)
+
+
+# Function to remove all text after the last valid hashtag
+def remove_after_last_valid_hashtag(text, hashtags):
+    index = -1
+    last_valid_hashtag_position = -1
+    for i in range(len(hashtags)):
+        position = text.rfind(hashtags[i])
+        if position != -1 and last_valid_hashtag_position < position:
+            last_valid_hashtag_position = position
+            index = i
+    if last_valid_hashtag_position != -1:
+        text = text[: last_valid_hashtag_position + len(hashtags[index])]
+    return text
+
+
+# Main function to clean up the text using all three steps
+def cleanup_text(text, hashtags=None):
+    # valid_hashtags = get_valid_hashtags(text, hashtags)
+    text = remove_italics(text)  # Remove italic text
+    text = remove_emojis(text)  # Remove emojis
+    if hashtags:
+        text = remove_after_last_valid_hashtag(text, hashtags)
+    text = text.strip()
+    # for hashtag
+    return text
+
+
+def cleanup_text_in_json(message, hashtags=None):
+    message["text"] = cleanup_text(message["text"], hashtags)
+    return message
+
+
+#################### UTILS END
+
+
+class InternalMessageStatus:
+    CREATED = 1
+    DOWNLOADING_MEDIA = 2
+    READY = 3
+
+    def stringify(status):
+        return [0, "CREATED", "DOWNLOADING", "READY"][status]
+
+
+class InternalMessage:
+    required_fields = ["id", "date"]
+    optional_fields = ["group_id", "text", "media"]
+
+    def __init__(self, **kwargs):
+        for field in InternalMessage.required_fields:
+            if field in kwargs:
+                setattr(self, field, kwargs[field])
+            else:
+                raise TypeError(f"Missing required argument: '{field}'")
+
+        for field in InternalMessage.optional_fields:
+            if field in kwargs:
+                setattr(self, field, kwargs[field])
+            else:
+                setattr(self, field, None)
+
+        self.update_status(InternalMessageStatus.CREATED)
+
+    def update_status(self, status):
+        self.update_time()
+        self.status = status
+        logger.debug(
+            f"Updated status to message {self.id} to {InternalMessageStatus.stringify(self.status)}"
+        )
+
+    def update_time(self):
+        self.last_update = time.time()
+
+    def __str__(self):
+        return f"""InternalMessage(
+    id = {self.id},
+    date = {self.date},
+    group_id = {self.group_id},
+    text = {self.text},
+    media = {self.media},
+    status = {InternalMessageStatus.stringify(self.status)}
+)"""
+
+
+class MessageDownloader:
+    def __init__(self, **kwargs):
+        self.__set_default_values()
+        self._set_fields(**kwargs)
+        self.fetching_done = asyncio.Event()
+
+    required_fields = ["api_id", "api_hash"]
+    optional_fields = [
+        "url",
+        "bot_token",
+        "phone",
+        "image_path",
+        "video_path",
+        "hashtags",
+        "start_date",
+    ]
+
+    def __set_required_fields(self, **kwargs):
+        for field in MessageDownloader.required_fields:
+            if field in kwargs:
+                setattr(self, field, kwargs[field])
+                logger.debug(f"{self}: Set required field '{field}' to {kwargs[field]}")
+            elif not getattr(self, field, None):
+                raise TypeError(f"Missing required argument: '{field}'")
+
+    def __set_optional_fields(self, **kwargs):
+        for field in MessageDownloader.optional_fields:
+            if field in kwargs:
+                setattr(self, field, kwargs[field])
+                logger.debug(f"{self}: Set optional field '{field}' to {kwargs[field]}")
+            elif not getattr(self, field, None):
+                setattr(self, field, None)
+
+    def _set_fields(self, **kwargs):
+        self.__set_required_fields(**kwargs)
+        self.__set_optional_fields(**kwargs)
+
+    def __set_default_values(self):
+        self.parsed_messages = []
+        self.single_messages = {}
+        self.group_messages = {}
+        self.ignored_group_ids = []
+
+        self.latest_group_id = None
+
+        self.url = "example.com/api/post"
+        self.image_path = "media/images"
+        self.video_path = "media/videos"
+        self.hashtags = [
+            "#срочно",
+            "#происшествия",
+            "#дтп",
+            "#лайфстайл",
+            "#спорт",
+            "#город",
+            "#политика",
+            "#развлечения",
+            "#18+",
+            "#топновости",
+        ]
+
+    # Проверка типа медиа (изображение или видео)
+    def get_media_path(self, message):
+        if message.photo:
+            return self.image_path
+        elif message.video or (
+            message.document and "video" in message.document.mime_type
+        ):
+            return self.video_path
+        return None
+
+    async def _process_media(self, message):
+        downloaded_media = is_media_downloaded(
+            message.id, self.image_path, self.video_path
+        )
+        if downloaded_media:
+            media_filename = downloaded_media[0].name
+            logger.info(f"Skipped downloading {media_filename}")
+            return {"filename": f"{media_filename}", "spoiler": message.media.spoiler}
+            # Media file is already downloaded before, return old file
+            # Proposal: Check if files were tampered with
+
+        media = None
+        media_temp_path = await message.download_media()
+        if media_temp_path:
+            # Получаем расширение файла
+            _, ext = os.path.splitext(media_temp_path)
+            ext = ext.lstrip(".")  # Убираем точку
+            media_filename = f"{message.id}.{ext}"
+
+            # Определяем, является ли файл изображением или видео
+            media_path = self.get_media_path(message)
+            if media_path:
+                media_destination = os.path.join(media_path, media_filename)
+                try:
+                    await asyncio.to_thread(
+                        shutil.move, media_temp_path, media_destination
+                    )
+                    logger.info(f"Downloaded media to {media_destination}")
+                    media = {
+                        "filename": f"{media_filename}",
+                        "spoiler": message.media.spoiler,
+                    }
+
+                except Exception as e:
+                    logger.warn(f"Failed to move media: {e}")
+                finally:
+                    if os.path.exists(media_temp_path):
+                        os.remove(media_temp_path)
+        return media
+
+    async def _process_message(self, message):
+        group_id = message.grouped_id
+
+        # If message doesn't have text and not in a group
+        if not (message.text or group_id):
+            return
+
+        # If message has text and no hashtags -> SKIP
+        if not check_message_text_for_hashtags(message.text, self.hashtags):
+            if group_id:
+                self.ignored_group_ids.append(group_id)
+            logger.info(
+                f"No valid hashtags found for message {message.id}. Group ID {group_id} ignored."
+            )
+            return
+
+        await asyncio.sleep(1)
+
+        if group_id and group_id in self.ignored_group_ids:
+            logger.info(f"GroupID {group_id} is likely an ad post.")
+            return
+
+        message_date = message.date
+        iso_date = message_date.isoformat()
+
+        internal_message = InternalMessage(
+            id=message.id,
+            group_id=group_id,
+            date=iso_date,
+            text=message.text,
+        )
+
+        if internal_message.group_id:
+            if internal_message.group_id in self.group_messages.keys():
+                self.group_messages[internal_message.group_id].append(internal_message)
+            else:
+                self.group_messages[internal_message.group_id] = [internal_message]
+        else:
+            self.single_messages[internal_message.id] = internal_message
+
+        if message.media:
+            internal_message.update_status(InternalMessageStatus.DOWNLOADING_MEDIA)
+            internal_message.media = await self._process_media(message)
+        internal_message.update_status(InternalMessageStatus.READY)
+
+    # async def _process_message_get_history(self, message):
+    #     group_id = message.grouped_id
+    #     latest_group_id = self.latest_group_id
+
+    #     if group_id:
+
+    """
+    Returns an Iterable with messages ordered from oldest to newest
+    """
+
+    async def _get_messages(self, client, channel):
+        messages = []
+
+        start_date = datetime.fromisoformat(self.start_date)
+        end_date = datetime.now()
+
+        async for message in client.iter_messages(
+            channel,
+            offset_date=end_date,
+            min_id=1,
+            limit=None,
+        ):
+            if message.date.replace(tzinfo=None) < start_date:
+                break
+            messages.append(message)
+
+        return reversed(messages)
+
+    async def fetch_messages(self, client, channel):
+        try:
+            messages = await self._get_messages(client, channel)
+            logger.info(
+                f"Messages up to {self.start_date} are downloaded and ready for processing."
+            )
+
+            tasks = []
+            for message in messages:
+                task = asyncio.create_task(self._process_message(message))
+                tasks.append(task)
+                # # HACK: создание небольшой задержки для обработки сообщений по порядку
+                await asyncio.sleep(0.1)
+
+            if tasks:
+                await asyncio.gather(*tasks)
+
+        except Exception as e:
+            logger.error(f"Error fetching messages: {e}")
+
+        finally:
+            self.fetching_done.set()
+
+    async def blm_event_handler(self, event):
+        logger.debug(event)
+        if event.message:
+            await self._process_message(event.message)
+
+    async def __send_one_message(self, converted_message: dict):
+        self.parsed_messages.append(converted_message)
+        await send_to_api(self.url, converted_message)
+
+    def convert_message_to_json_generator(self, transform: callable):
+        return lambda message: cleanup_text_in_json(transform(message), self.hashtags)
+
+    async def __send_prepared_messages(
+        self, messages: dict, condition: callable, transform: callable
+    ):
+        keys_to_remove = [key for key in messages if condition(messages[key])]
+
+        for key in keys_to_remove:
+            message = messages[key]
+            transformed_message = transform(message)
+            await self.__send_one_message(transformed_message)
+            logger.debug(f"Removing message {key}")
+            del messages[key]
+
+    async def __send_messages_cycle(self):
+        DELAY_IN_SECONDS = 3
+
+        await self.__send_prepared_messages(
+            self.single_messages,
+            lambda message: message.status == InternalMessageStatus.READY,
+            self.convert_message_to_json_generator(convert_message_to_data),
+        )
+
+        await self.__send_prepared_messages(
+            self.group_messages,
+            lambda group: all(
+                message.status == InternalMessageStatus.READY
+                and message.last_update < time.time() - DELAY_IN_SECONDS
+                for message in group
+            ),
+            self.convert_message_to_json_generator(convert_group_to_data),
+        )
+
+    async def send_messages(self):
+        while not self.fetching_done.is_set():
+            await asyncio.sleep(5)
+            await self.__send_messages_cycle()
+
+        # ew
+        for _ in range(3):
+            if not (self.single_messages or self.group_messages):
+                break
+            await self.__send_messages_cycle()
+            await asyncio.sleep(3)
+
+        # EWWWWWWWWWWWW
+        await self.__send_prepared_messages(
+            self.group_messages,
+            lambda group: all(
+                message.status == InternalMessageStatus.READY for message in group
+            ),
+            convert_group_to_data,
+        )
+
+    def is_all_fields_present(self, *args):
+        return all(getattr(self, field, None) for field in args)
+
+    async def get_history(self, channel):
+        # required_fields = ['api_id', 'api_hash', 'phone']
+        create_output_directories(self.image_path, self.video_path)
+
+        client = await TelegramClient(
+            f"load_session_{self.api_id}",
+            self.api_id,
+            self.api_hash,
+        ).start(self.phone)
+
+        logger.info("`get_history()` session started and user authorized.")
+        tasks = [
+            asyncio.create_task(self.fetch_messages(client, channel)),
+            asyncio.create_task(self.send_messages()),
+        ]
+        await asyncio.gather(*tasks)
+        await client.disconnect()
+        write_messages_to_file(self.parsed_messages, f"{channel}.json")
+
+    async def get_new_messages(self, channel):
+        create_output_directories(self.image_path, self.video_path)
+
+        client = await TelegramClient(
+            f"blm_session_{self.api_id}",
+            self.api_id,
+            self.api_hash,
+        ).start(bot_token=self.bot_token)
+
+        # await client(JoinChannelRequest(channel))
+
+        client.add_event_handler(
+            self.blm_event_handler, events.NewMessage(chats=channel)
+        )
+        logger.info("`get_new_messages()` session started and user authorized.")
+        task = asyncio.create_task(self.send_messages())
+        await client.run_until_disconnected()
---
+++ blm.py
@@ -0,0 +1,82 @@
+import argparse
+import configparser
+import asyncio
+from TelegramDownloader import MessageDownloader
+
+
+def convert_to_number_if_possible(a, just_try=True):
+    try:
+        return int(a)
+    except Exception:
+        try:
+            return float(a)
+        except Exception:
+            try:
+                return int(a, 16)
+            except Exception:
+                if just_try:
+                    return a
+                else:
+                    raise
+
+
+def number_config(config):
+    ret_cfg = {}
+    for sk, sv in config._sections.items():
+        ret_cfg[sk] = {k: convert_to_number_if_possible(v) for k, v in sv.items()}
+    return ret_cfg
+
+
+def load_arguments():
+    parser = argparse.ArgumentParser(description="Load credentials from .ini file.")
+    parser.add_argument("-c", "--config", help="Path to the .ini configuration file")
+    parser.add_argument(
+        "-d",
+        "--dry",
+        action="store_true",
+        help="Run the script without making any calls to API",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def load_config(filename):
+    config = configparser.ConfigParser()
+    config.read(filename)
+    return config
+
+
+def get_config_from_arguments(args):
+    if args.config:
+        return load_config(args.config)
+    return None
+
+
+def overwrite_default_credentials(config: dict):
+    if config:
+        global api_id, api_hash, phone_number
+        api_values = {
+            key: config["API"].get(key)
+            for key in ("api_id", "api_hash", "phone_number")
+        }
+        if all(api_values.values()):
+            api_id, api_hash, phone_number = api_values.values()
+
+
+async def main():
+    try:
+        args = load_arguments()
+        config = get_config_from_arguments(args)
+    except Exception as e:
+        print(f"Failed to load config, exiting. Error message: {e}")
+        exit()
+
+    channel = convert_to_number_if_possible(config.get("info", "channel"))
+    md = MessageDownloader(
+        **config["tg"], **config["paths"], start_date=config["info"]["start_date"]
+    )
+    await md.get_new_messages(channel)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
